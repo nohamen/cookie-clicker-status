8ede2e57d76bd6e0e0396b9cba41234edb87fd5e
hamen <noeln4363@gmail.com>
2025-10-20 19:44:30 -0400

    Update StatusFile.py

--- StatusFile.py
+++ StatusFile.py
@@ -1,3 +1,6 @@
 javascript:document.write("JavaImport;") ("See's Your Status For Your Current Bakery And Status ".)
 
 [ask-and-respond] ("Your Current Status Is {Staus} And Your Bakery Is {Bakery}".)
+
+# install This Using 
+pit install cookieclickerstatus
